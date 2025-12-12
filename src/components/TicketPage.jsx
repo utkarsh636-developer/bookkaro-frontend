dbd11faae324bde56ec853be400a47a5e12b4cfb
user.email like: "utkarsh63.kankal@gmail.com <utkarsh63.kankal@gmail.com>
2025-12-13 03:56:01 +0530

    added convenience fee in ticket page

--- src/components/TicketPage.jsx
+++ src/components/TicketPage.jsx
@@ -108,7 +108,8 @@ function TicketPage() {
   const { event, quantity, totalPrice } = eventData;
 
   const GST_RATE = 0.18;
-  const totalPriceWithGST = totalPrice * (1 + GST_RATE);
+  const CONVENIENCE_FEE_RATE = 0.02;
+  const totalPriceWithGST = totalPrice * (1 + GST_RATE + CONVENIENCE_FEE_RATE);
 
   return (
     <div className="bg-gray-100 min-h-screen flex flex-col items-center justify-center px-4 py-8">
